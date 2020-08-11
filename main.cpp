88fe39d02829a1ef080315531d2db0ba8260931b
Daniel León <65270757+DanielLeonD@users.noreply.github.com>
2020-08-10 20:44:22 -0500

    3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,53 @@
-#include <iostream>
+#include "filehandler.h"
+#include "parking.h"
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main(){
+
+    MainProg Prog;
+    FileHandler Data;
+
+
+    if(Data.LoadData())
+        Data.getData(Prog.getMainData(), Prog.getFloorsData(), Prog.getAdminsData(), Prog.getUsersData());
+
+    else{
+        Prog.Setup();
+        Data.setData(Prog.getAdminsData());
+    }
+
+    while(Prog.getIsRunning()){
+
+        while(Prog.getIsAdmin() && !Prog.exit()){
+
+            Prog.AdminMenu();
+
+            if(Prog.doSaveAdmins()){
+                Data.setData(Prog.getAdminsData());
+                Prog.setSaveAdmins(false);
+            }
+
+            if(Prog.doSaveData()){
+
+                Prog.getRate();
+                Prog.getSavePresets(Data.getSaveType(), Data.getEncryptData(), Data.getHash());
+                Prog.setSaveData(false);
+
+            }
+        }
+
+        while(!Prog.getIsAdmin() && !Prog.exit()){
+
+            Prog.UserMenu();
+
+            if(Prog.doSaveUsers())
+                Data.setData(Prog.getMainData());
+                Data.setData(Prog.getFloorsData());
+
+                Prog.setSaveUsers(false);
+        }
+    }
+
+    Data.Destroy();
+
 }
